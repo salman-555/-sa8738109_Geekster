1008b8fc40d0aa5837a3314e0b828acc7042fe6c
salman-555 <112814601+salman-555@users.noreply.github.com>
2022-09-06 08:12:26 +0530

    Add files via upload

---
+++ Day_03_home_work.java
@@ -0,0 +1,56 @@
+public class Day_03_home_work {
+    
+
+    public static void main (String[] args){
+       /*  Store the result of the following expressions in ans variable of type boolean and print
+the final result.  */
+
+    //1)
+    boolean ans = 40>=2*45 || 30>=2*10;
+    System.out.println("1)  "+ans);
+    //2)
+    boolean an= 40>3 && 20>3;
+    System.out.println("2)  "+an);
+    //3)
+    boolean anso= 50>7 && 30>=40;
+    System.out.println("3)  "+anso);
+    //4)
+    boolean as= 50<25 || 30>2;
+    System.out.println("4)  "+as);
+    //5)
+    boolean answ= 70<=75 || 40<=2;
+    System.out.println("5)  "+answ);
+    //6)
+    boolean answe = !( 45==35);
+    System.out.println("6)  "+answe);
+    //7)
+    boolean answer= (20<32 && 2!=30) && (35>=20 || 35!=25);
+    System.out.println("7)  "+answer);
+    //8)
+    boolean asw= !(20>=30);
+    System.out.println("8)  "+asw);
+    //9)
+    boolean aso= !(30>40);
+    System.out.println("9)  "+aso);
+    //10)
+    boolean asoo = !(40==2*20) && 75==15*5;
+    System.out.println("10) "+asoo);
+    //11)
+    boolean ass = !(40>=40) || (50>=2*25);
+    System.out.println("11) "+ass);
+    //12)
+    boolean asww = !(10*5==50) || (2*3==7 || 9==28/3);
+    System.out.println("12) "+asww);
+    //13)
+    boolean answee = (20*5==100 || 10!=10) && (30*2==60 || 50<40);
+    System.out.println("13) "+answee);
+    //14)
+    boolean answerr = ( !(90>=40) && !(80>36) );
+    System.out.println("14) "+answerr);
+    //15)
+    boolean ansee = ( (50>=20) || 90>2*45) && ( 30!=2*15);
+    System.out.println("15) "+ansee);
+
+
+    }
+}
